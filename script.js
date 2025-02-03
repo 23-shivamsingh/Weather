26127bedb6d6272415750abb6f5a3094de7b21ae
23-shivamsingh <shivams2307@gmail.com>
2025-02-04 00:28:27 +0530

    Added API

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search_box");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+
+search.addEventListener("click",() => {
+    const APIkey = "863856e601fa1712bd9a7ed93ac5b1f2";
+    
+});
